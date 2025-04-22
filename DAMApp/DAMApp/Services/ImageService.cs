dcfdc8082a995bdf973e4bf87133ca039ae0ccd5
Emil Jørgensen <emiljorgensen05@gmail.com>
2025-04-22 11:46:29 +0200

    refactor: enhance image management with sorting and tagging features wip

--- DAMApp/DAMApp/Services/ImageService.cs
+++ DAMApp/DAMApp/Services/ImageService.cs
@@ -1,5 +1,6 @@
 using DAMApp.Components.Pages;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.VisualBasic;
 
 namespace DAMApp.Services;
 
@@ -66,8 +67,7 @@ public static class ImageService
 				apiUrl =
 					$"api/v1/assets/imageIdPileFromSearch?size={size}&offset={pageNumber}&searchquery={searchText}";
 			}
-
-
+			
 			imageIds = await Http.GetFromJsonAsync<List<string>>(apiUrl);
 
 		}
@@ -94,13 +94,94 @@ public static class ImageService
 		}
 		return imageSources;
 	}
-/*
+
 	public static async Task<List<string>> GetImagesByProduct(string productId)
 	{
 		List<string> ImagesByProduct = new List<string>();
 
+		try
+		{
+			HttpClientHandler handler = new HttpClientHandler();
+			HttpClient Http = new HttpClient(handler)
+			{
+				BaseAddress = new Uri("http://localhost:5115/") // Replace with your API's base URL
+			};
+
+			string apiUrl = $"api/v1/assets/{productId}/all";
+
+			GetProductAssetsIdsResponse response = await Http.GetFromJsonAsync<GetProductAssetsIdsResponse>(apiUrl);
+			foreach (Guid guid in response.ImageIds)
+			{
+				ImagesByProduct.Add(guid.ToString());
+			}
+
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error: {ex.Message}");
+		}
+		
+		return ImagesByProduct;
+	}
+	
+	public static async Task<List<string>> GetTags()
+	{
+		List<string> tags = new List<string>();
+
+		try
+		{
+			HttpClientHandler handler = new HttpClientHandler();
+			HttpClient Http = new HttpClient(handler)
+			{
+				BaseAddress = new Uri("http://localhost:5115/")
+			};
+
+			string apiUrl = "api/v1/assets/tags";
+			tags = await Http.GetFromJsonAsync<List<string>>(apiUrl);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error: {ex.Message}");
+		}
+
+		return tags;
+	}
+	
+	public static async Task UpdatePriority()
+	{
+		var payload = new Assets.ImageUploadWithoutProduct
+		{
+			//Content = dataUrl
+		};
+
+		// Make a new HttpClient
+		using HttpClientHandler handler = new HttpClientHandler();
+		using HttpClient Http = new HttpClient(handler)
+		{
+			BaseAddress = new Uri("http://localhost:5115/") // Replace with your API's base URL
+		};
+		// Post to the backend via HTTP
+		var response = await Http.PostAsJsonAsync("api/v1/assets/add", payload);
+
+		if (response.IsSuccessStatusCode)
+		{
+			Console.WriteLine("Image uploaded successfully.");
+		}
+		else
+		{
+			var error = await response.Content.ReadAsStringAsync();
+			Console.WriteLine($"Error: {response.StatusCode} - {error}");
+		}
 
 	}
-	*/
 
+	class GetProductAssetsIdsResponse {
+
+		public List<Guid> ImageIds { get; set; }
+
+		public GetProductAssetsIdsResponse(List<Guid> imageIds)
+		{
+			ImageIds = imageIds;
+		}
+	}
 }
